f2a7add3ea3b851b1409b6f738e6a88257db46d2
Gavin Whelan <gavwhela@gmail.com>
2017-04-05 01:09:53 -0400

    Basic commenting stuff.

--- Handler/Posts.hs
+++ Handler/Posts.hs
@@ -4,7 +4,10 @@ import Import
 
 getPostR :: PostId -> Handler Html
 getPostR postId = do
+  muser <- maybeAuthId
   post <- runDB $ get404 postId
+  comments <- runDB $ selectList [ CommentPostId ==. postId ] []
+  (formWidget, enctype) <- generateFormPost $ newCommentForm muser postId
   defaultLayout $ do
         setTitle . toHtml $ postTitle post
         $(widgetFile "post")
@@ -30,9 +33,30 @@ postNewPostR = do
                setTitle "New Post"
                $(widgetFile "new-post")
 
+postPostR :: PostId -> Handler Html
+postPostR postId = do
+  muser <- maybeAuthId
+  post <- runDB $ get404 postId
+  comments <- runDB $ selectList [ CommentPostId ==. postId ] []
+  ((res, formWidget), enctype) <- runFormPost $ newCommentForm muser postId
+  case res of
+    FormSuccess entry -> do
+        runDB $ insert entry
+        setMessage "Successfully created comment"
+        redirect (PostR postId)
+    _ -> defaultLayout $ do
+               setTitle . toHtml $ postTitle post
+               $(widgetFile "post")
+
 newPostForm :: UTCTime -> Form Post
 newPostForm currentTime = renderDivs $ Post
   <$> areq textField "Title" Nothing
   <*> areq textField "Author" Nothing
   <*> pure currentTime
   <*> areq textField "Body" Nothing
+
+newCommentForm :: Maybe UserId -> PostId -> Form Comment
+newCommentForm muser postId = renderDivs $ Comment
+  <$> areq textField "Message" Nothing
+  <*> pure muser
+  <*> pure postId
