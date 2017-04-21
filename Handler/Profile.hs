6c283ef08e2bbf7e33afc80ed9ed221d03ab2ebb
Gavin Whelan <gavwhela@gmail.com>
2017-04-21 11:20:51 -0400

    Kinda awful hack for password reset without entering username.

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,10 +1,12 @@
 module Handler.Profile where
 
 import Import
+import Yesod.Auth.Account (resetPasswordR)
 
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
+    csrfToken <- fmap reqToken getRequest
     defaultLayout $ do
         setTitle . toHtml $ userUsername user <> "'s User page"
         $(widgetFile "profile")
